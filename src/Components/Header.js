83221561dd818a34aae3ed85a8a4a77bc8fd05ee
satya960 <satya9604166@gmail.com>
2024-02-13 00:07:15 +0530

    back button code added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const Header = () => {
-  return (
-    <>
-     <div className='w-full h-12 bg-green-600 flex items-center justify-between  '>
-        <img className='h-12 w-12  ' src='https://cdn4.vectorstock.com/i/1000x1000/95/88/sign-symbol-health-logo-hospital-red-cross-vector-35679588.jpg'/>
-        <h1 className='font-extrabold text-2xl'>Satya Health Centre</h1>
-        <button className='h-12 w-12 bg-red-500 rounded-xl hover:bg-red-50'>Back</button>
-     </div>
-    </>
-  )
-}
+  const handleBackButtonClick = () => {
+    window.history.back(); // Navigate back using window.history object
+  };
 
-export default Header ;
+  return (
+    <div className='w-full h-12 bg-green-600 flex items-center justify-between'>
+      <img className='h-12 w-12' src='https://cdn4.vectorstock.com/i/1000x1000/95/88/sign-symbol-health-logo-hospital-red-cross-vector-35679588.jpg' alt="Hospital Logo" />
+      <h1 className='font-extrabold text-2xl'>Satya Health Centre</h1>
+      <button className='h-12 w-12 bg-red-500 rounded-xl hover:bg-red-50' onClick={handleBackButtonClick}>Back</button>
+    </div>
+  );
+};
+
+export default Header;
